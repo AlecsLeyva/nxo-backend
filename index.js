1db10c74ab9eef1c702747cc4720bf90d7a82c85
AlecsLeyva <04.leyva.ortega@gmail.com>
2026-03-12 03:50:57 -0600

    Update server routes and add user registration

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // --- RUTAS DEL SERVIDOR ---
 
-// 1. Ruta de prueba para ver si el servidor está encendido
+// 1. Ruta base: Ver si el servidor está encendido
 app.get('/', (req, res) => {
     res.send('¡El servidor central de NXO Teatro está funcionando perfectamente!');
 });
 
-// 2. Ruta para probar si logró entrar a tu base de datos en Supabase
+// 2. Ruta de prueba: Conexión a la base de datos
 app.get('/probar-bd', async (req, res) => {
     const { data, error } = await supabase.from('usuarios').select('*');
     if (error) {
@@ -30,11 +30,25 @@ app.get('/probar-bd', async (req, res) => {
     res.json({ mensaje: "Conexión a Supabase EXITOSA", datos: data });
 });
 
-// 3. NUEVA: Guardar Reserva (La usa tu App de Android)
+// 3. NUEVA: Guardar Usuario (Tu requerimiento #3, la usa la App de Android)
+app.post('/usuarios', async (req, res) => {
+    const { email, password } = req.body;
+    
+    // Le decimos a Supabase que inserte una nueva fila en la tabla 'usuarios'
+    const { data, error } = await supabase
+        .from('usuarios')
+        .insert([{ email, password }]);
+        
+    if (error) {
+        return res.status(400).json({ exito: false, error: error.message });
+    }
+    res.json({ exito: true, mensaje: 'Usuario guardado en la nube exitosamente' });
+});
+
+// 4. Guardar Reserva (La usa tu App de Android)
 app.post('/reservar', async (req, res) => {
     const { obra, horario, asiento, usuario_email } = req.body;
     
-    // Le decimos a Supabase que inserte una nueva fila en la tabla 'reservas'
     const { data, error } = await supabase
         .from('reservas')
         .insert([{ obra, horario, asiento, usuario_email }]);
@@ -45,11 +59,10 @@ app.post('/reservar', async (req, res) => {
     res.json({ exito: true, mensaje: 'Reserva guardada en la nube exitosamente' });
 });
 
-// 4. NUEVA: Validar QR (La usará tu ESP32/Arduino en el torniquete físico)
+// 5. Validar QR (La usará tu ESP32/Arduino en el torniquete físico)
 app.post('/validar-qr', async (req, res) => {
     const { obra, asiento } = req.body;
     
-    // Buscamos si existe ese asiento exacto para esa obra en Supabase
     const { data, error } = await supabase
         .from('reservas')
         .select('*')
